d292a71c21d0547be53a2841b02088d3762b309c
chamindu <chamindudilshan80@gmail.com>
2025-03-11 18:29:12 +0530

    adding files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,75 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import Category from './components/Category';
+// import Products from './components/Products';
+import { fetcher } from './Fetcher';
+
 
 function App() {
+  const [categories,setCategories]=useState({errorMessage:'',data:[]});
+  const [products,setProducts]=useState([]);
+
+  useEffect(()=>
+  {
+    const fetchData=async ()=>{
+    const responseObject =await fetcher("/categories");
+    setCategories(responseObject);
+  }
+  fetchData();
+  },[])
+
+
+
+
+  const handleCategoryClick= id =>{
+    {
+      const fetchData=async ()=>{
+      const responseObject =await fetcher("/products?catId"+id);
+      setProducts(responseObject);
+    }
+    fetchData();
+  }
+
+
+  const renderCategories= ()=>{
+    return categories.data.map(c=>
+      <Category key={c.id} id={c.id} title={c.title} onCategoryClick={()=>handleCategoryClick(c.id)}/>
+    );
+  }
+
+const renderProducts=()=>{
+  return products.map(p=>
+    <div> {p.title}</div>
+  
+  );
+
+}
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <header><p>My Store</p></header>
+    <section> 
+      <nav> 
+      {categories.errorMessage && <div>Error :{categories.errorMessage}</div>}
+      {  categories.data && renderCategories()  }
+    
+      </nav>
+      <article>
+        <h1>Products</h1>
+        {products && renderProducts()}
+      </article>
+    </section>
+
+    <footer>
+      footer
+    </footer>
+
+
+    </>
   );
 }
 
---
+++ src/components/Products.js
@@ -0,0 +1,9 @@
+// import React from 'react'
+
+// const Products=({title})=>{
+//     return(
+//         <div>{title}</div>
+//     )
+// }
+
+// export default Products;
